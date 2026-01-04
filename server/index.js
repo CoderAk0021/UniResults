b78b649cdf8d5b7a9861398658adac65c5766698
CoderAk0021 <akcoder2122@gmail.com>
2026-01-04 19:55:57 +0530

    Restricted origin to app

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: '*', // Allow all origins (we will restrict this later for security)
+    origin: 'https://university-results.vercel.app', // Allow all origins (we will restrict this later for security)
     credentials: true
 }));
 
