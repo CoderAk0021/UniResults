a23f9ffa078ba94ea789d8c32ccb736af0d1e0ca
Anup Mandal <125991648+CoderAk0021@users.noreply.github.com>
2026-02-08 20:12:23 +0530

    Comment out admin registration route
    Comment out the registration route for admin.

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,13 +1,12 @@
 import express from "express";
 
 import {
-  handleAdminLogin,
-  handleAdminRegister,
+  handleAdminLogin
 } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
 router.post("/login", handleAdminLogin);
-router.post("/register", handleAdminRegister);
+//router.post("/register", handleAdminRegister);
 
 export default router;
